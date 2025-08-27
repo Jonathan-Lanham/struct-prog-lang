5d8215b9d5ec88e17c7f299f868b9e8555306abb
Jonathan-Lanham <Jonathanlanham80@gmail.com>
2025-08-27 16:16:53 -0400

    updated tokenizer and added parser file

--- topic-01-integers/tokenizer.py
+++ topic-01-integers/tokenizer.py
@@ -1,11 +1,16 @@
 import re
 
 patterns = [
+    [r"print", "print"],
     [r"\d*\.\d+|\d+\.\d*|\d+","number"], #\d = digit, + means one or more, * means 0 or more
+    [r"[a-zA-z_][a-zA-Z0-9_]*", "identifier"], # Indentifiers
     [r"\+", "+"],
     [r"\-", "-"],
     [r"\*", "*"],
     [r"\/", "/"],
+    [r"\(", "("],
+    [r"\)", ")"],
+    [r"\s+", "whitespace"],
     [r".", "error"]
 ]
 
@@ -35,7 +40,11 @@ def tokenize(characters):
             else:
                 token["value"] = int(value)
 
-        tokens.append(token)
+        if token["tag"] == "identifier":
+            token["value"] = value
+
+        if token["tag"] != "whitespace":
+            tokens.append(token)
         position = match.end()
 
     tokens.append({"tag":None, "position":position})
@@ -43,38 +52,36 @@ def tokenize(characters):
 
 def test_simple_tokens():
     print("test simple tokens")
-    assert tokenize("+") == [
-        {"tag":"+", "position":0},
-        {"tag":None, "position":1}
-    ]
 
-    assert tokenize("-") == [
-        {"tag":"-", "position":0},
-        {"tag":None, "position":1}
-    ]
-
-    assert tokenize("*") == [
-        {"tag":"*", "position":0},
-        {"tag":None, "position":1}
-    ]
-
-    assert tokenize("/") == [
-        {"tag":"/", "position":0},
-        {"tag":None, "position":1}
-    ]
+    for c in "+-*/()":
+        assert tokenize(c) == [
+            {"tag":c, "position":0},
+            {"tag":None, "position":1}
+        ]
 
     assert tokenize("3") == [
         {"tag":"number", "position":0, "value": 3},
         {"tag":None, "position":1}
     ]
 
-    assert tokenize("3.1") == [
-        {"tag":"number", "position":0, "value": 3.1},
-        {"tag":None, "position":1}
+    assert tokenize("cat") == [
+        {"tag":"identifier", "position":0, "value": "cat"},
+        {"tag":None, "position":3}
     ]
+
+def test_simple_expressions():
+    print("test simple expressions")
+    t = tokenize("2+3")
+    assert t == [{'tag': 'number', 'position': 0, 'value': 2}, {'tag': '+', 'position': 1}, {'tag': 'number', 'position': 2, 'value': 3}, {'tag': None, 'position': 3}]
+
+def test_whitespace():
+    print("test simple expressions")
+    t = tokenize("1 + 2")
+    assert t == [{'tag': 'number', 'position': 0, 'value': 1}, {'tag': '+', 'position': 2}, {'tag': 'number', 'position': 4, 'value': 2}, {'tag': None, 'position': 5}]
 
 if __name__ == "__main__":
     print("testing tokenizer")
     test_simple_tokens()
-
+    test_simple_expressions()
+    test_whitespace()
     print("done")
